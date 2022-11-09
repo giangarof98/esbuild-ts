e33fb28282eda1e6a5d0e875b8163f824354ecce
Gianmarco G <giangarof98@gmail.com>
2022-11-09 17:20:37 -0500

    set up tbook

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    const ref = useRef<any>()
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const startService = async() => {
+        try{
+            ref.current = await esbuild.initialize({
+                worker: true,
+                wasmURL: '/esbuild.wasm'
+            })
+        } catch(e){
+            console.error(e)
+        }
+    }
+
+    useEffect(() => {
+        startService()
+    }, [])
+
+    const onClick = async () => {
+        // if(!ref.current){
+        //     return
+        // }
+
+        //try{
+            // const res = await esbuild.transform(input, {
+            //     loader: 'jsx', 
+            //       target: 'es2015'
+            
+            // });
+            const res = await esbuild.build({
+                entryPoints: ['index.js'],
+                bundle: true,
+                write: false,
+                plugins: [unpkgPathPlugin()]
+
+            })
+
+            console.log(res)
+            setCode(res.outputFiles[0].text);
+        // }catch(err){
+        //     console.error(err);
+        // }
+    
+    }
+
+    return <div>
+                <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+                <div>
+                    <button onClick={onClick}>Submit</button>
+                </div>
+                <pre>{code}</pre>
+            </div>
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<App/>)
