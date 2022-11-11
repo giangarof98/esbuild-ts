420dec759d48edb63d68237c1eeef62cebf2641d
Gianmarco G <giangarof98@gmail.com>
2022-11-10 20:29:29 -0500

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
+import { fetchPlugin } from './plugins/fetch-plugin';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
@@ -38,11 +39,19 @@ const App = () => {
                 entryPoints: ['index.js'],
                 bundle: true,
                 write: false,
-                plugins: [unpkgPathPlugin()]
+                plugins: [
+                    unpkgPathPlugin(),
+                    fetchPlugin(input),
+
+                ],
+                define: {
+                    'process.env.NODE_ENV':'"production"',
+                    global: 'window',
+                }
 
             })
 
-            console.log(res)
+            //console.log(res)
             setCode(res.outputFiles[0].text);
         // }catch(err){
         //     console.error(err);
