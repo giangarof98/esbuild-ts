750b501d003f77d91995d8412fa8a13b0cede630
Gianmarco G <giangarof98@gmail.com>
2022-11-29 21:42:10 -0500

    resize horizontally

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from "react";
-
+import './preview.css';
 interface PreviewProps {
     code: string
 }
@@ -35,7 +35,16 @@ const Preview: React.FC<PreviewProps> = ({code}) => {
 
     }, [code])
 
-    return <iframe title='preview' ref={iframe} sandbox='allow-scripts' srcDoc={html}/>
+    return (
+        <div className="preview-wrapper">
+            <iframe
+                title='preview' 
+                ref={iframe} 
+                sandbox='allow-scripts' 
+                srcDoc={html}
+            />
+        </div>
+    )
 }
 
 export default Preview;
