841cb8745bcf24c4fb8913d4b696d4a946176ac0
Gianmarco G <giangarof98@gmail.com>
2022-12-01 19:47:38 -0500

    debug the editor style

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,10 +18,13 @@ const CodeCell = () => {
         <Resizable direction="vertical">
 
             <div style={{height:'100%', display:'flex', flexDirection:'row'}}>
-                <CodeEditor 
-                    initialValue='const a = 1'
-                    onChange={(value) => setInput(value)}
-                />
+                <Resizable direction="horizontal">
+                    <CodeEditor 
+                        initialValue='const a = 1'
+                        onChange={(value) => setInput(value)}
+                    />
+
+                </Resizable>
                 {/* <div>
                     <button onClick={onClick}>Submit</button>
                 </div> */}
