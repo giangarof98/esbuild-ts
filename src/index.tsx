dc7afaf7fb3833a2a2c19d4edff0c94d5e3dd0da
Gianmarco G <giangarof98@gmail.com>
2022-12-02 21:16:35 -0500

    text-editor component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from "react-dom/client";
-import CodeCell from './components/code-cell';
+//import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
 
-    return <div>
-                <CodeCell/>
-            </div>
+    return (
+        <div>
+            <TextEditor />
+            {/* <CodeCell/> */}
+        </div>
+    )
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement);
 
 root.render(<App/>)
